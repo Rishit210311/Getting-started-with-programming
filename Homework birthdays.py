a705b364d69fa6a458a62118240fd6899bb57122
Rishit210311 <shailesh1.pandey@gmail.com>
2025-04-05 09:18:09 +0100

    Add files via upload

---
+++ Homework birthdays.py
@@ -0,0 +1,13 @@
+
+andrew = "May 14"
+sachin = "August 23"
+dhoni = "December 1"
+rohit = "March 19"
+sam = "October 30"
+
+
+print("Andrew's birthday is on", andrew)
+print("Sachin's birthday is on", sachin)
+print("Dhoni's birthday is on", dhoni)
+print("Rohit's birthday is on", rohit)
+print("Sam's birthday is on", sam)
